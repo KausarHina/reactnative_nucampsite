1ad2b7a7af9411deb7b79d3cf126312ca10aad35
Kausar Hina <kausar.hina@gmail.com>
2024-04-16 12:50:35 -0600

    Navigation Part 3

--- shared/partners.js
+++ shared/partners.js
@@ -1,39 +1,34 @@
-import bootStrapLogo from '../assets/img/bootstrap-logo.png';
-import gitLogo from '../assets/img/git-logo.png';
-import mongoLogo from '../assets/img/mongo-logo.png';
-import nodeLogo from '../assets/img/node-logo.png';
-
 export const PARTNERS = [
     {
         id: 0,
         name: 'Bootstrap Outfitters',
-        image: bootStrapLogo,
+        image: require("../assets/images/bootstrap-logo.png"),
         featured: false,
         description:
-            "Bootstrap Outfitters supplies you with the gear you need at prices you can't beat."
+            "Bootstrap Outfitters supplies you with the gear you need at prices you can't beat.",
     },
     {
         id: 1,
         name: 'Git Out Expeditions',
-        image: gitLogo,
+        image: require("../assets/images/git-logo.png"),
         featured: false,
         description:
-            'Join Git Out Expeditions to explore new horizons with a group of other adventurers!'
+            'Join Git Out Expeditions to explore new horizons with a group of other adventurers!',
     },
     {
         id: 2,
         name: 'Mongo Fly Shop',
-        image: mongoLogo,
+        image: require("../assets/images/mongo-logo.png"),
         featured: false,
         description:
-            'Need a new fishing pole, a tacklebox, or flies of all kinds? Stop by Mongo Fly Shop.'
+            'Need a new fishing pole, a tacklebox, or flies of all kinds? Stop by Mongo Fly Shop.',
     },
     {
         id: 3,
         name: 'Node Outdoor Apparel',
-        image: nodeLogo,
+        image: require("../assets/images/node-logo.png"),
         featured: true,
         description:
-            'From polar fleeces to swimsuits, hiking boots to waders, a visit to Node will be sure to get you covered.'
-    }
+            'From polar fleeces to swimsuits, hiking boots to waders, a visit to Node will be sure to get you covered.',
+    },
 ];
